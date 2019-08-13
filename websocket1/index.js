24783b3858427811a9ad0f49a892da0314fbd5b4
Jared4Robertson <jared4robertson@gmail.com>
2019-08-13 16:03:19 -0400

    big changes

--- websocket1/index.js
+++
@@ -1,8 +0,0 @@
-const api = require('binance');
-
-const binanceWS = new api.BinanceWS(true); // Argument specifies whether the responses should be beautified, defaults to true
-
-binanceWS.onDepthUpdate('BNBBTC', (data) => {
-    console.log(data);
-
-});
