905934bf514fe1f46b80246c2a5a38abdb1ee3a6
Jared4Robertson <jared4robertson@gmail.com>
2019-08-24 12:43:33 -0400

    Big moves

---
+++ index.js
@@ -0,0 +1,26 @@
+function ToggleFunFacts() {
+    var x = document.getElementById("FunFacts");
+    if (x.style.display == "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+  function ToggleProjects() {
+    var x = document.getElementById("FunFacts");
+    if (x.style.display == "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+  function ToggleSkills() {
+    var x = document.getElementById("FunFacts");
+    if (x.style.display == "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+
+  
